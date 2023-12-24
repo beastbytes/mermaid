649d6c64dc8c4d2fec3aee4682eb7b66dd65549e
Chris Yates <chris@clyates.plus.com>
2023-12-24 21:07:44 +0000

    Add Mermaid JavaScript

--- src/Mermaid.php
+++ src/Mermaid.php
@@ -8,15 +8,23 @@ declare(strict_types=1);
 
 namespace BeastBytes\Mermaid;
 
+use phpDocumentor\Reflection\Types\Self_;
+
 class Mermaid
 {
     public const INDENTATION = '  ';
+    public const JS = "<script type=\"module\">\n    import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs'\n    mermaid.initialize(%s)\n</script>";
     public const MERMAID = "<pre class=\"mermaid\">\n%s\n</pre>";
 
     public static function create(string $name, array $parameters = []): MermaidInterface
     {
         $fqcn = __NAMESPACE__ . str_repeat("\\$name", 2);
         return new $fqcn(...$parameters);
+    }
+
+    public static function js(array $config = ['startOnLoad' => true]): string
+    {
+        return sprintf(self::JS, json_encode($config));
     }
 
     public static function render(string $mermaid): string
