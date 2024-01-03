6c017bf9e32e918b36f5f7f1f82a67b39d4ca5ee
Chris Yates <chris@clyates.plus.com>
2024-01-03 23:25:41 +0000

    Add Direction.php

---
+++ src/Direction.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @copyright Copyright © 2023 BeastBytes - All rights reserved
+ * @license BSD 3-Clause
+ */
+
+declare(strict_types=1);
+
+namespace BeastBytes\Mermaid;
+
+enum Direction
+{
+    case BT;
+    case LR;
+    case RL;
+    case TB;
+}
