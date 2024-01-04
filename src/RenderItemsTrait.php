33bc061e42e3af289aec95863c30a65ca92df006
Chris Yates <chris@clyates.plus.com>
2024-01-04 13:03:40 +0000

    Add RenderItemsTrait.php

---
+++ src/RenderItemsTrait.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright Copyright © 2024 BeastBytes - All rights reserved
+ * @license BSD 3-Clause
+ */
+
+declare(strict_types=1);
+
+namespace BeastBytes\Mermaid;
+
+trait RenderItemsTrait
+{
+    private function renderItems(array $items, string $indentation): string
+    {
+        $output = [];
+
+        foreach ($items as $item) {
+            $output[] = $item->render($indentation . Mermaid::INDENTATION);
+        }
+
+        return implode("\n", $output);
+    }
+}
