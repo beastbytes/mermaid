5936e2729c6472d72fd75d79300e589f4bc6f8ea
Chris <chris@clyates.plus.com>
2025-12-28 16:52:54 +0000

    Update TitleTrait.php

--- src/TitleTrait.php
+++ src/TitleTrait.php
@@ -7,6 +7,7 @@ namespace BeastBytes\Mermaid;
 trait TitleTrait
 {
     public const bool QUOTE = true;
+    private const string TITLE = 'title %s%s%1$s';
 
     private ?string $title = null;
 
@@ -20,7 +21,7 @@ trait TitleTrait
     public function renderTitle(string $indentation, bool $quote = false): string
     {
         return is_string($this->title)
-            ? sprintf('%stitle %s%s%2$s', $indentation, ($quote ? '"' : ''), $this->title)
+            ? $indentation . sprintf(self::TITLE, ($quote ? '"' : ''), $this->title)
             : ''
         ;
     }
