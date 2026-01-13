b303c9099dfb9f6b6d0b579b5d5b99f8b51cb308
Chris Yates <beastbytes@users.noreply.github.com>
2026-01-13 19:40:02 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,10 @@ author = 'BeastBytes'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinxcontrib.phpdomain']
+extensions = [
+  'sphinx-rtd-theme',
+  'sphinxcontrib.phpdomain'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
